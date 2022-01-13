0127f90c5e4dd1a581d7f4827e0b06cec15bc9a6
Ashish Singh <ashishsingh4u@gmail.com>
2022-01-14 02:14:53 +0800

    Refactored

--- src/providers/MessageParser.js
+++ src/providers/MessageParser.js
@@ -7,17 +7,21 @@ class MessageParser {
   parse(message) {
     const lowerCaseMessage = message.toLowerCase();
 
-    if (lowerCaseMessage.includes("hello")) {
+    if (lowerCaseMessage.includes("hello") || lowerCaseMessage.includes("hi")) {
       this.actionProvider.greet();
-    } else if (lowerCaseMessage.includes("front end")) {
+    } else if (lowerCaseMessage.includes("frontend")) {
       this.actionProvider.handleFrontEnd();
-    } else if (lowerCaseMessage.includes("back end")) {
+    } else if (lowerCaseMessage.includes("backend")) {
       this.actionProvider.handleBackEnd();
     } else if (lowerCaseMessage.includes("database")) {
       this.actionProvider.handleDatabase();
     } else if (lowerCaseMessage.includes("deployment")) {
       this.actionProvider.handleDeployment();
-    } else if (lowerCaseMessage.includes("continuous integration")) {
+    } else if (
+      lowerCaseMessage.includes("continuous integration") ||
+      lowerCaseMessage.includes("ci") ||
+      lowerCaseMessage.includes("cd")
+    ) {
       this.actionProvider.handleContinuosIntegration();
     } else {
       this.actionProvider.unknown();
